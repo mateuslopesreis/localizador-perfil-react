ad7fc5b86c953d3fe7eec93f0f0e92e4b09f25d5
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2023-04-13 22:01:38 -0300

    utilizando o hook TanStack

---
+++ src/pages/userPerfil/index.tsx
@@ -0,0 +1,13 @@
+import { QueryClientProvider, useQuery } from "@tanstack/react-query"
+import { getUser } from "../../services/api"
+import { QueryClient } from "react-query"
+
+
+
+   
+
+
+export function UserPerfil(){
+   
+    
+}
